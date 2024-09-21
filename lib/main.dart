df34630a3928e5341a574013c09795aaf089d5e6
mohammedmaher <mohammedmaher3333333@gmail.com>
2024-09-21 19:04:11 +0300

    create splash view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
+import 'package:book_verse/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
-  runApp(const BookVerseApp ());
+  runApp(const BookVerseApp());
 }
 
-class BookVerseApp  extends StatelessWidget {
-  const BookVerseApp ({super.key});
+class BookVerseApp extends StatelessWidget {
+  const BookVerseApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      //home: SplachView(),
+    return const GetMaterialApp(
+      home: SplashView(),
     );
   }
 }
