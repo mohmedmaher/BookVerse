1a1b0aea23b47759b23474d30aa4b165b8ed34b0
mohammedmaher <mohammedmaher3333333@gmail.com>
2024-09-22 22:31:17 +0300

    styles text done

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,12 +1,27 @@
+import 'package:book_verse/constant.dart';
 import 'package:flutter/cupertino.dart';
 
-abstract class Styles{
-  static const  textStyle18=TextStyle(
+abstract class Styles {
+  static const titleStyle30 = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
+  );
+  static const titleStyle20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
+  );
+  static const textStyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
   );
-  static const titleStyle30=TextStyle(
-    fontSize: 30,
+  static const titleStyle16 = TextStyle(
+    fontSize: 16,
     fontWeight: FontWeight.w600,
+  );
+  static const titleStyle14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
   );
 }
