772b15cc4880bc1484e6c6194363af61b8619e2c
mohammedmaher <mohammedmaher3333333@gmail.com>
2024-09-24 02:15:00 +0300

    create aoi service

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,16 @@
+import 'package:book_verse/core/errors/failures.dart';
+import 'package:book_verse/features/home/data/models/book_model/book_model.dart';
+import 'package:book_verse/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    throw UnimplementedError();
+  }
+}
