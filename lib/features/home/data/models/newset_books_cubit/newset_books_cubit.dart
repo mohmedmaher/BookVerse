18a6d3a66f4124053a238a60fbac54d0ff6b9cdc
mohammedmaher <mohammedmaher3333333@gmail.com>
2024-09-24 15:07:01 +0300

    newset books cubit done

--- lib/features/home/data/models/newset_books_cubit/newset_books_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:book_verse/features/home/data/models/book_model/book_model.dart';
-import 'package:meta/meta.dart';
-
-part 'newset_books_state.dart';
-
-class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
-}
