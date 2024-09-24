37acec5c6fb2fdf974f0c6ac23a7865ec51ad955
mohammedmaher <mohammedmaher3333333@gmail.com>
2024-09-25 02:06:46 +0300

    fetch similar books

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,7 +1,13 @@
+import 'package:book_verse/core/utils/service_locator.dart';
+import 'package:book_verse/features/home/data/models/book_model/book_model.dart';
+import 'package:book_verse/features/home/data/repos/home_repo_impl.dart';
+import 'package:book_verse/features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart';
 import 'package:book_verse/features/home/presentation/views/book_details_view.dart';
 import 'package:book_verse/features/home/presentation/views/home_view.dart';
 import 'package:book_verse/features/search/presentation/views/search_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+
 import '../../features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
@@ -21,7 +27,12 @@ abstract class AppRouter {
       ),
       GoRoute(
         path: kBookDetailsView,
-        builder: (context, state) => const BookDetailsView(),
+        builder: (context, state) => BlocProvider(
+          create: (context) => SimilarBooksCubit(getIt.get<HomeRepoImpl>()),
+          child: BookDetailsView(
+            bookModel: state.extra as BookModel,
+          ),
+        ),
       ),
       GoRoute(
         path: kSearchView,
--- lib/features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,21 +1,38 @@
+import 'package:book_verse/core/widgets/custom_error_widget.dart';
+import 'package:book_verse/core/widgets/custom_loading_indicator.dart';
+import 'package:book_verse/features/home/presentation/manger/similar_books_cubit/similar_books_cubit.dart';
 import 'package:book_verse/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListView extends StatelessWidget {
   const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.2,
-      child: ListView.builder(
-        itemCount: 6,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) => const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 5.0),
-          child: CustomBookImage(imageUrl: 'https://t4.ftcdn.net/jpg/01/10/04/51/360_F_110045173_QgmA3gg5OwTlLNQBqmPdFnkh6sPvsvt8.jpg',),
-        ),
-      ),
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.2,
+            child: ListView.builder(
+              itemCount: 6,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) => const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5.0),
+                child: CustomBookImage(
+                  imageUrl:
+                      'https://t4.ftcdn.net/jpg/01/10/04/51/360_F_110045173_QgmA3gg5OwTlLNQBqmPdFnkh6sPvsvt8.jpg',
+                ),
+              ),
+            ),
+          );
+        }else if(state is SimilarBooksFailure){
+          return CustomErrorWidget(errMessage: state.errMessage);
+        }else{
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
