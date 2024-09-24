f0859741618412c836c3e93efd93fc4e7789136a
mohammedmaher <mohammedmaher3333333@gmail.com>
2024-09-24 15:02:11 +0300

    create newset books cubit state

---
+++ lib/features/home/data/models/newset_books_cubit/newset_books_state.dart
@@ -0,0 +1,20 @@
+part of 'newset_books_cubit.dart';
+
+@immutable
+sealed class NewsetBooksState {}
+
+final class NewsetBooksInitial extends NewsetBooksState {}
+
+final class NewsetBooksLoading extends NewsetBooksState {}
+
+final class NewsetBooksFailure extends NewsetBooksState {
+  final String errMessage;
+
+  NewsetBooksFailure(this.errMessage);
+}
+
+final class NewsetBooksSuccess extends NewsetBooksState {
+  final List<BookModel> books;
+
+  NewsetBooksSuccess(this.books);
+}
